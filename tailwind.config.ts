e731e45946afb30368bb1f02c8cc493ddaa3f257
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-08-22 20:17:39 +0530

    feat: :sparkles: add logo

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,11 @@ const config: Config = {
 		"./app/**/*.{js,ts,jsx,tsx,mdx}",
 	],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"primary-dark": "#292B35",
+			},
+		},
 	},
 	plugins: [],
 };
