eb1dc646578a3a2d18e1ccd778019f427a255da9
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-08-25 17:59:47 +0530

    feat: :sparkles: add feature card

---
+++ components/home/feature-section/feature-section.tsx
@@ -0,0 +1,49 @@
+import FeatureImageWheel from "@/assets/images/home/feature-section-wheel.png";
+import FeatureImageRefer from "@/assets/images/home/feature-section-refer.png";
+import FeatureImagePlay from "@/assets/images/home/feature-section-play.png";
+import { FeatureCard } from "./elements";
+
+const FeatureData = [
+	{
+		id: 1,
+		image: FeatureImageWheel,
+		title: "Spin To Win",
+		description:
+			"Get Ready to Win! Give the wheel a spin for a shot a 500 Dream Gamez Token. Your prizes are ready",
+		buttonText: "Spin Now",
+	},
+	{
+		id: 2,
+		image: FeatureImageRefer,
+		title: "Refer More, Win More",
+		description:
+			"Get Ready to Win! Give the wheel a spin for a shot a 500 Dream Gamez Token. Your prizes are ready",
+		buttonText: "Spin Now",
+	},
+	{
+		id: 3,
+		image: FeatureImagePlay,
+		title: "Play Free, Earn Big",
+		description:
+			"Get Ready to Win! Give the wheel a spin for a shot a 500 Dream Gamez Token. Your prizes are ready",
+		buttonText: "Spin Now",
+	},
+];
+
+const FeatureSection = () => {
+	return (
+		<div className="w-full flex flex-wrap items-center justify-center px-6 my-10">
+			{FeatureData.map((data) => (
+				<FeatureCard
+					key={data.id}
+					image={data.image}
+					buttonText={data.buttonText}
+					headerText={data.title}
+					description={data.description}
+				/>
+			))}
+		</div>
+	);
+};
+
+export default FeatureSection;
