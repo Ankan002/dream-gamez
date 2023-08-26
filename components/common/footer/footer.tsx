ea025a8c71c3f7157d944a80125863a53edbe2af
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-08-26 23:08:18 +0530

    feat: :sparkles: add footer container

---
+++ components/common/footer/footer.tsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+	return (
+		<footer className="w-full flex flex-col py-3 bg-secondary-dark">
+			<div className="w-full bg-primary-dark md:px-10 px-4 py-2">
+				<p className="text-sm text-white">
+					Copyright &#169; 2023
+					<span className="text-primary-yellow mx-1">DreamGameZ.</span>
+					All Rights Reserved.
+				</p>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
